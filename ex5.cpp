a043fda43ff1a37da03bd8936bf8ba2fb7ec72ab
HigorGomesLima <37384951+HigorGomesLima@users.noreply.github.com>
2019-05-20 23:22:31 -0300

    Update ex5.cpp

--- ex5.cpp
+++ ex5.cpp
@@ -35,30 +35,30 @@ void objeto(void){
 void placa(float x,float y,float w,float h,float r,float g,float b){
     glBegin(GL_POLYGON);
         glColor3f(r,g,b);
-        glVertex2f(x,(y+(w/4)));
+        glVertex2f(x,(y+(w/2)));
         glVertex2f((x+w),y);
-        glVertex2f(x,(y-(w/4)));
+        glVertex2f(x,(y-(w/2)));
         glVertex2f((x-w),y);
     glEnd();
     glBegin(GL_POLYGON);
         glColor3f(0.5,0.5,0.5);
-        glVertex2f(x,(y-(w/4)));
+        glVertex2f(x,(y-(w/2)));
         glVertex2f((x-w),y);
         glVertex2f((x-w),(y-h)); //-2 -0.5
-        glVertex2f(x,(y-(w/4)-h));//0 0-1-0.5
+        glVertex2f(x,(y-(w/2)-h));//0 0-1-0.5
     glEnd();
     glBegin(GL_POLYGON);
         glColor3f(0.4,0.4,0.4);
         glVertex2f((x+w),y);
-        glVertex2f(x,(y-(w/4)));
-        glVertex2f(x,(y-(w/4)-h));
+        glVertex2f(x,(y-(w/2)));
+        glVertex2f(x,(y-(w/2)-h));
         glVertex2f((x+w),(y-h));
     glEnd();
     glBegin(GL_LINE_LOOP);
         glColor3f(0,0,0);
-        glVertex2f(x,(y+(w/4)));
+        glVertex2f(x,(y+(w/2)));
         glVertex2f((x+w),y);
-        glVertex2f(x,(y-(w/4)));
+        glVertex2f(x,(y-(w/2)));
         glVertex2f((x-w),y);
     glEnd();
 }
@@ -71,12 +71,12 @@ static void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glColor3d(1,0,0);
-    float t = 2;
+    float t = 4;
     for(int i = 0;i < 4;i++){
         for(int j=3;j >= 0;j--){
                 if(mesa[i][j] == 1){
-                    float x = -(t*1.5) + j*t/2 + i*t/2;
-                    float y = j*t/4 - i*t/4;
+                    float x = -(t*1.5) + j*t + i*t;
+                    float y = j*t/2 - i*t/2;
                     placa(x,y,t,0.5,0,0,1);
                 }
         }
@@ -102,7 +102,7 @@ int main(int argc, char *argv[])
     glutDisplayFunc(display);
 
     glClearColor(1,1,1,1);
-    glOrtho(-20,20,-10,10,-10,10);
+    glOrtho(-25,25,-25,25,-25,25);
     glutMainLoop();
 
     return EXIT_SUCCESS;
