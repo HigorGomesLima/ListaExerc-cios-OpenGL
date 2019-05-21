477f4e4d3c27b3c1f3be243c893cf4eef21de2fb
HigorGomesLima <37384951+HigorGomesLima@users.noreply.github.com>
2019-05-20 23:16:06 -0300

    Create ex5.cpp

---
+++ ex5.cpp
@@ -0,0 +1,109 @@
+#include <windows.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <stdio.h>
+#include <stdlib.h>
+
+int mesa[4][4];
+
+void objeto(void){
+    glBegin(GL_POLYGON);
+        glColor3f(0,0,1);
+        glVertex2f(0,1);
+        glVertex2f(2,0);
+        glVertex2f(0,-1);
+        glVertex2f(-2,0);
+    glEnd();
+    glBegin(GL_POLYGON);
+        glColor3f(0.5,0.5,1);
+        glVertex2f(0,-1);
+        glVertex2f(-2,0);
+        glVertex2f(-2,-0.5);
+        glVertex2f(0,-1.5);
+    glEnd();
+    glBegin(GL_POLYGON);
+        glColor3f(0.5,0.5,1);
+        glVertex2f(2,0);
+        glVertex2f(0,-1);
+        glVertex2f(0,-1.5);
+        glVertex2f(2,-0.5);
+    glEnd();
+}
+void placa(float x,float y,float w,float h,float r,float g,float b){
+    glBegin(GL_POLYGON);
+        glColor3f(r,g,b);
+        glVertex2f(x,(y+(w/4)));
+        glVertex2f((x+w),y);
+        glVertex2f(x,(y-(w/4)));
+        glVertex2f((x-w),y);
+    glEnd();
+    glBegin(GL_POLYGON);
+        glColor3f(0.5,0.5,0.5);
+        glVertex2f(x,(y-(w/4)));
+        glVertex2f((x-w),y);
+        glVertex2f((x-w),(y-h)); //-2 -0.5
+        glVertex2f(x,(y-(w/4)-h));//0 0-1-0.5
+    glEnd();
+    glBegin(GL_POLYGON);
+        glColor3f(0.4,0.4,0.4);
+        glVertex2f((x+w),y);
+        glVertex2f(x,(y-(w/4)));
+        glVertex2f(x,(y-(w/4)-h));
+        glVertex2f((x+w),(y-h));
+    glEnd();
+    glBegin(GL_LINE_LOOP);
+        glColor3f(0,0,0);
+        glVertex2f(x,(y+(w/4)));
+        glVertex2f((x+w),y);
+        glVertex2f(x,(y-(w/4)));
+        glVertex2f((x-w),y);
+    glEnd();
+}
+/*
+Fórmula 3D - X e Y
+x = -1.5 + y*0,5 + i*0,5;
+y = y*0,25 - x*0,25;
+*/
+static void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3d(1,0,0);
+    float t = 2;
+    for(int i = 0;i < 4;i++){
+        for(int j=3;j >= 0;j--){
+                if(mesa[i][j] == 1){
+                    float x = -(t*1.5) + j*t/2 + i*t/2;
+                    float y = j*t/4 - i*t/4;
+                    placa(x,y,t,0.5,0,0,1);
+                }
+        }
+    }
+    glutSwapBuffers();
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(640,480);
+    glutInitWindowPosition(0,0);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+    for(int i = 0;i < 4;i++){
+        for(int j=0;j < 4;j++){
+            if(i < j)
+                mesa[i][j] = 1;
+            else
+                mesa[i][j] = 1;
+        }
+    }
+    glutCreateWindow("Lista CG");
+    glutDisplayFunc(display);
+
+    glClearColor(1,1,1,1);
+    glOrtho(-20,20,-10,10,-10,10);
+    glutMainLoop();
+
+    return EXIT_SUCCESS;
+}
